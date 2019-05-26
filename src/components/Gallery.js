03919f05abd3753d1fc34203aa2dbd8f6c5272d5
Kiran Adhikari <kiruu1238@gmail.com>
2019-05-26 23:37:09 +0545

    Logic for Pause and Play added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,47 @@
 import React, { Component } from 'react';
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Playingurl: '',
+            audio: null,
+            playing: false
+        }
+    }
+    playMusic(url) {
+        let audio = new Audio(url);
+        if (!this.state.playing) {
+            audio.play();
+            this.setState({
+                playing: true,
+                Playingurl: url,
+                audio
+            })
+        }
+        else {
+            if (this.state.Playingurl === url) {
+                this.state.audio.pause();
+                this.setState({
+                    playing: false
+                })
+            }
+            else {
+                this.state.audio.pause();
+                audio.play();
+                this.setState({
+                    playing: true,
+                    Playingurl: url,
+                    audio
+                })
+
+            }
+        }
+
+    }
     render() {
         const { track } = this.props;
+        console.log(track)
 
         if (track) {
             return (
@@ -14,6 +53,7 @@ class Gallery extends Component {
                                 <div
                                     key={k}
                                     className="track"
+                                    onClick={() => this.playMusic(track.preview_url)}
                                 >
                                     <img
                                         src={img}
